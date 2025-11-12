7ffbd9bd8d07cf07c71111e1659bb46e9fba283e
ralphdosreis <ralphdosreistorres@gmail.com>
2025-11-12 14:44:46 -0300

    null e undefined

---
+++ null-e-undefined/script.js
@@ -0,0 +1,24 @@
+"use strict";
+// null é um tipo primitivo que representa a ausência de valor. Comum em funções do DOM que fazem uma busca, mas retornam null quando não são bem sucedidas
+const button = document.querySelector('button');
+const config = localStorage.getItem('config');
+if (button !== null) {
+    button.click();
+}
+if (button) {
+    button.click();
+}
+if (button)
+    button.click();
+button?.click();
+console.log(typeof null);
+// undefined representa variáveis/propriedades que foram instanciadas, porém os seus valores ainda não foram definidos
+let total;
+console.log(total); // undefined
+const data = {};
+const livro = {};
+const jogo = {
+    nome: 'Ragnarok',
+};
+console.log(livro.nome?.toLowerCase());
+console.log(jogo.nome?.toLowerCase());
