8ffe57c7ca0ea29d76b38f5ac186ada3aeb8889a
ralphdosreis <ralphdosreistorres@gmail.com>
2025-11-12 14:45:51 -0300

    any

---
+++ any/script.js
@@ -0,0 +1,23 @@
+"use strict";
+// any indica que o dado pode conter qualquer tipo de dado no Typescript.
+// Deve-se evitar o uso do any, pois o mesmo remove todas as seguranças e conveniências que o TS fornece
+function normalizar(texto) {
+    return texto.trim().toLowerCase();
+}
+console.log(normalizar(' DesigN'));
+function mostrarCursos(cursos) {
+    cursos.forEach((curso) => {
+        document.body.innerHTML += `
+    <div>
+    <h2>${curso.nome}<\h2>
+    <p>Horas: ${curso.horas}<\p>
+    <\div>`;
+    });
+}
+const dados = 'o any gera problemas';
+const cursosDisponiveis = [
+    { nome: 'Ralph', horas: 26 },
+    { nome: 'Luiz', horas: 24 },
+    { nome: 'Bruna', horas: 18 },
+];
+mostrarCursos(cursosDisponiveis);
